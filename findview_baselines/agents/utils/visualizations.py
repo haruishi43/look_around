960611a43fd73ebdf0e5174264f2fbb6a414d436
Toraudonn <toraudonn@gmail.com>
2021-10-18 06:45:21 +0000

    add double length for basic

--- findview_baselines/agents/utils/visualizations.py
+++ findview_baselines/agents/utils/visualizations.py
@@ -2,3 +2,27 @@
 
 """Agent specific visualizations
 """
+
+import cv2
+import numpy as np
+
+
+def draw_matches(
+    gray_pers: np.ndarray,
+    kps_pers,
+    gray_target: np.ndarray,
+    kps_target,
+    matches,
+) -> np.ndarray:
+
+    img = cv2.drawMatches(
+        img1=gray_pers,
+        keypoints1=kps_pers,
+        img2=gray_target,
+        keypoints2=kps_target,
+        matches1to2=matches,
+        outImg=None,
+        flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+    )
+
+    return img
