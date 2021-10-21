c1d6366ff6b97b03bc9ac6563efeb9e53fcc173d
Toraudonn <toraudonn@gmail.com>
2021-10-21 03:59:36 +0000

    change parameters for sift

--- configs/findview/sun360/benchmarks/orb_alpha_indoor.py
+++ configs/findview/sun360/benchmarks/orb_alpha_indoor.py
@@ -1,7 +1,7 @@
 _base_ = [
     '../../../_base_/datasets/sun360_alpha_indoor.py',
     '../../../_base_/envs/findview.py',
-    '../agents/feature_matching.py',
+    '../agents/orb.py',
     './base.py',
 ]
 fm = dict(
