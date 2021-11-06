3f36e7ab05c3e032ab111df42bf3c4d0ef177e06
Toraudonn <toraudonn@gmail.com>
2021-11-06 08:56:37 +0000

    add double for outdoor

---
+++ configs/findview/sun360/rl/inverse_2_double_alpha_outdoor.py
@@ -0,0 +1,17 @@
+_base_ = [
+    './inverse_2_alpha_outdoor.py',
+]
+trainer = dict(
+    identifier='double',
+    num_envs=32,
+    num_updates=15000,
+    ckpt_interval=500,
+    log_interval=10,
+)
+validator = dict(
+    num_envs=16,
+)
+scheduler = dict(
+    initial_difficulty='easy',
+    update_interval=5000,
+)
