ec5561b85bb4563803797b5ea50fc98d85ff614c
Toraudonn <toraudonn@gmail.com>
2021-11-11 17:20:14 +0000

    forgot to update default.py

--- configs/corrupted_findview/sun360/alpha/outdoor/_base_/defaults.py
+++ configs/corrupted_findview/sun360/alpha/outdoor/_base_/defaults.py
@@ -2,3 +2,10 @@ _base_ = [
     '../../../../../_base_/datasets/sun360/alpha_outdoor.py',
     '../../../../_base_/env.py',
 ]
+corrupter = dict(
+    corruptions='all',
+    severity=0,
+    bounded=False,
+    use_clear=True,
+    deterministic=False,
+)
