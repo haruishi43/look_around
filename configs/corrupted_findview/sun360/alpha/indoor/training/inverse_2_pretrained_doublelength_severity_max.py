e7d52f41c0addeac452caa4a4b9927292612228b
Toraudonn <toraudonn@gmail.com>
2021-11-12 04:21:35 +0000

    add doublelength pretrained severity max

---
+++ configs/corrupted_findview/sun360/alpha/indoor/training/inverse_2_pretrained_doublelength_severity_max.py
@@ -0,0 +1,24 @@
+_base_ = [
+    './inverse_2.py',
+]
+trainer = dict(
+    identifier='adapted_doublelength',
+    num_updates=30000,
+    ckpt_interval=1000,
+    log_interval=10,
+    pretrained='./pretrained/run_2_doublelength/ckpt.28.pth',
+)
+scheduler = dict(
+    initial_difficulty='easy',
+    update_interval=5000,
+)
+corrupter = dict(
+    corruptions='all',
+    severity=5,
+    bounded=False,
+    use_clear=True,
+)
+corruption_scheduler = dict(
+    initial_severity=5,
+    max_severity=5,
+)
