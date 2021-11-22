0d3450878e880fd1fb66ed5660a5d96cc837a662
Toraudonn <toraudonn@gmail.com>
2021-11-22 14:16:22 +0000

    convert checkpoint code (mycv to mmcv)

---
+++ tools/convert_mycv2mmcv_ckpt.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+import torch
+
+from mmcv import Config
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='convert')
+    parser.add_argument('ckpt', help='ckpt path')
+    args = parser.parse_args()
+
+    ckpt_path = args.ckpt
+
+    assert os.path.exists(ckpt_path)
+    d = torch.load(ckpt_path, map_location='cpu')
+
+    d['cfg'] = Config.fromstring(d['cfg'].pretty_text, file_format='.py')
+    torch.save(d, ckpt_path)
+
